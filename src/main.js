8aa36a45432cef6987702417a13ac8cfa29a897f
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-02-12 20:58:21 +0200

    Added some styles for the resopnsive design

--- src/main.js
+++ src/main.js
@@ -12,11 +12,11 @@ calculateBtn.addEventListener("click", (e) => {
     alert("Please fill in both names");
   } else {
     if (lovePercentage > 0 && lovePercentage <= 35) {
-      displayOutPut.innerHTML = `${name1} and ${name2} love percentage is ${lovePercentage} <br /> <hr />  Don't Match, Don't worry; every love story has its unique challenges. Face them together!`;
+      displayOutPut.innerHTML = `${name1} and ${name2} love percentage is ${lovePercentage}% <br /> <hr />  Don't Match, Don't worry; every love story has its unique challenges. Face them together!`;
     } else if (lovePercentage > 35 && lovePercentage <= 50) {
-      displayOutPut.innerHTML = `${name1} and ${name2} love percentage is ${lovePercentage} <br/>  <hr /> A strong foundation of friendship is the key to lasting love. Keep nurturing your bond and watch it blossom into something beautiful`;
+      displayOutPut.innerHTML = `${name1} and ${name2} love percentage is ${lovePercentage}% <br/>  <hr /> A strong foundation of friendship is the key to lasting love. Keep nurturing your bond and watch it blossom into something beautiful`;
     } else if (lovePercentage > 50 && lovePercentage <= 75) {
-      displayOutPut.innerHTML = `${name1} and ${name2} love percentage is ${lovePercentage} <br/> <hr />  You both have a good chance at making it work. Keep the love alive and watch it grow!`;
+      displayOutPut.innerHTML = `${name1} and ${name2} love percentage is ${lovePercentage}% <br/> <hr />  You both have a good chance at making it work. Keep the love alive and watch it grow!`;
     } else {
       displayOutPut.innerHTML = `${name1} and ${name2} love percentage is ${lovePercentage} <br/> Congratulations! You've found your soulmate, Prepare for a magical journey together.`;
     }
